43e410868f56be2abea0598d7a38951ab6c602b8
Ent <rainbow.punk.treant@gmail.com>
2020-12-16 22:57:26 -0800

    save point

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import os
 import sys
 import pygame
+import subprocess
+
+from distutils.core import setup
 
 
 
 class Joystick():
+   
     pygame.init()
     pygame.joystick.init()
     def detectButton(self, stick):
@@ -13,31 +17,37 @@ class Joystick():
                 if event.type == pygame.JOYBUTTONDOWN:
                     for button in range(stick.get_numbuttons()):
                         butt = stick.get_button(button)
+                        # if butt == 1:
+                            # print("button"+str(button)+" down")
                         
-                        print("button"+str(butt)+" down")
                 if event.type == pygame.JOYBUTTONUP:
                     for button in range(stick.get_numbuttons()):
                         butt = stick.get_button(button)
                         
-                        print("button"+str(butt)+" up")
-                        if button == 0 :
-                            sys.exit(1) 
+                        # print("button"+str(button)+" up")
+                        if butt == 0 and button == 0:
+                            subprocess.run('/usr/bin/yell')
+                            print("\x1b[48;2;20;50;20mRAWR\x1b[0m")
+                            #sys.exit(1) 
         else:
             sys.exit(1)               
 
     def showButtons(self):
 
         joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
-        stick = joysticks[0]
-        stick.init()
-        print("NUMBER OF BUTTONS::"+str(stick.get_numbuttons()))
-        #print(stick.get_name())
-        print("NUMBER OF hats::"+str(stick.get_numhats()))
+        for stick in joysticks:
+            uid = stick.get_guid()
+            # print(uid)
+            
+                # stick.init()
+                # print("NUMBER OF BUTTONS::"+str(stick.get_numbuttons()))
+                # #print(stick.get_name())
+                # print("NUMBER OF hats::"+str(stick.get_numhats()))
 
-        print("number ")
-        # for event in pygame.event.get():
-        # if event.type == pygame.JOYBUTTONDOWN:
-        return stick
+                # print("number ")
+                # # for event in pygame.event.get():
+                # # if event.type == pygame.JOYBUTTONDOWN:
+            return stick
 joy = Joystick()
 
 stick = joy.showButtons()
